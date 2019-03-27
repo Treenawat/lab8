8ce9528c997cdb3df60036c2b22a924c33e417b6
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-03-26 19:05:49 -0500

    Try

--- project08/src/Driver.java
+++ project08/src/Driver.java
@@ -1,5 +1,15 @@
 public class Driver{
 	public static void main(String[] args){
+	//Create
+	BoardGame test = new BoardGame();
+	
+	//Add Player
+	test.addPlayer("Winner", GamePiece.RED_RACER, Location.LOUNGE);
+	test.addPlayer("Loser", GamePiece.RED_THIMBLE, Location.STUDHALL);
+	
+	//Try
+	GamePiece playerPiece = test.getPlayerGamePiece("Winner");
+	System.out.print(playerPiece);
 	
 	}
 }
